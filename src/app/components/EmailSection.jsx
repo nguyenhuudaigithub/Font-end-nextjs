94214da4bda67bf871b676bba3dc01364cfcf649
NguyenHuuDai <nguyenhuudaihs@icloud.com>
2024-07-29 21:59:02 +0700

    begin-font-end = const

---
+++ src/app/components/EmailSection.jsx
@@ -0,0 +1,27 @@
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+const contact = {
+  title: "Let's Connect",
+  detail:
+    "I'm currently looking for new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I'll try my best to get back to you!",
+  socialMedia: [
+    {
+      name: "GitHub",
+      image: "/github-icon.svg",
+      link: "https://github.com",
+    },
+    {
+      name: "LinkedIn",
+      image: "/linkedin-icon.svg",
+      link: "https://linkedin.com",
+    },
+    // Add more social media platforms here
+  ],
+};
+
+const EmailSection = () => {};
+
+export default EmailSection;
---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const footer = {
+  logo: "DAINGUYEN",
+  text: "All rights reserved.",
+};
+
+const Footer = () => {
+  return (
+    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
+      <div className="container p-12 flex justify-between">
+        <span>{footer.logo}</span>
+        <p className="text-slate-600">{footer.text}</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
